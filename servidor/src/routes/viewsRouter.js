4f1ee7d98b146d14b53a866233b54a7aa67787d5
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2026-03-24 09:37:06 -0300

    se modifican las rutas

--- servidor/src/routes/viewsRouter.js
+++ servidor/src/routes/viewsRouter.js
@@ -1,27 +1,26 @@
 import { Router } from "express";
 /* import ProductManager from "../dao/ProductManager.js";
  */
-import  { productModel } from '../model/productModel.js'
+import { productModel } from "../model/productModel.js";
 const router = Router();
-
 
 /* const productManager = new ProductManager("products.json");
  */
 
-
 router.get("/tienda", async (req, res) => {
-    const { page= 1} = req.query
+  const { page = 1 } = req.query;
 
-    const pagination = await productModel.paginate({}, {
-        limit: 10,
-        page: page,
-        lean: true
-    });
-    console.log(pagination)
-    res.render("products", { pagination });
-
+  const pagination = await productModel.paginate(
+    {},
+    {
+      limit: 10,
+      page: page,
+      lean: true,
+    },
+  );
+  console.log(pagination);
+  res.render("products", { pagination });
 });
-
 
 /* router.get("/realtimeproducts", async (req, res) => {
     const { page = 1 } = req.query;
@@ -37,33 +36,43 @@ router.get("/tienda", async (req, res) => {
     res.render("realTimeProducts", { pagination });
 }); */
 router.get("/realtimeproducts", async (req, res) => {
-    const { page = 1 } = req.query;
+  const { page = 1 } = req.query;
 
-    const pagination = await productModel.paginate({}, {
-        limit: 10,
-        page: page,
-        lean: true
-    });
+  const pagination = await productModel.paginate(
+    {},
+    {
+      limit: 10,
+      page: page,
+      lean: true,
+    },
+  );
 
-    res.render("realTimeProducts", { pagination });
+  res.render("realTimeProducts", { pagination });
 });
 
-
 router.get("/", async (req, res) => {
-  const { page = 1 } = req.query;
-  const pagination = await productModel.paginate({}, {
-    limit: 10,
-    page: page,
-    lean: true
-  });
-  res.render("index", { pagination });
-/*   res.render("home", { pagination });
- */});
+  const { page = 1, category } = req.query;
+  let query = {};
+  if (category) {
+    query.category = category;
+  }
 
+  const pagination = await productModel.paginate(
+    {},
+    {
+      limit: 10,
+      page: page,
+      lean: true,
+    },
+  );
+  res.render("index", { pagination });
+  /*   res.render("home", { pagination });
+   */
+});
 
 router.get("/product/:pid", async (req, res) => {
-    const { pid } = req.params;
-    const product = await productModel.findById(pid).lean();
-    res.render("masInfoProducto", { product });
+  const { pid } = req.params;
+  const product = await productModel.findById(pid).lean();
+  res.render("masInfoProducto", { product });
 });
 export default router;
